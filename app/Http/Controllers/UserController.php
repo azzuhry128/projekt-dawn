eef0e5a1c55790f8a546e6dc489469487e1ebc4b
azzuhry128 <muhammadazzuhry04@gmail.com>
2023-05-05 10:23:08 +0700

    changes @UserController: - uuid as id generator - bcrypt as password encryption algorithm - response now contains passport && status object

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Account;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
+use Ramsey\Uuid\Uuid;
 
 class UserController extends Controller
 {
@@ -13,19 +15,22 @@ class UserController extends Controller
     }
     public function storeNewUser(Request $request)
     {
-        if ($request->input("request") == "accepted") {
+        $newUserId = Uuid::uuid4();
+
+        if ($request->input("status.request" ) == "accepted") {
 
             $newAccount = new Account;
 
+            $newAccount->id = $newUserId;
             $newAccount->username = $request->input("username");
             $newAccount->email = $request->input("email");
-            $newAccount->password = $request->input("password");
+            $newAccount->password = Hash::make($request->input("password"));
 
             $newAccount->save();
 
-            return response("data successfully inserted into database");
+            return response($request->only(['passport', 'status']));
         } else {
-            return response("duplicate detected");
+            return response($request->only(['passport', 'status']));
         }
 
 
