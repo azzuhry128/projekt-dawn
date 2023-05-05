8f399518d3a9288ff5265df278924fb6689e279f
azzuhry128 <muhammadazzuhry04@gmail.com>
2023-05-05 08:22:39 +0700

    registered middleware: - DoppelChecker - HelloWorld

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\DoppelChecker;
+use App\Http\Middleware\HelloWorld;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -35,6 +37,8 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
+            HelloWorld::class,
+            DoppelChecker::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
