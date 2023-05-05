18a20148a05763821e57f109586a830580d9d66f
azzuhry128 <muhammadazzuhry04@gmail.com>
2023-05-05 08:17:44 +0700

    added getRegistrationPage() && storeNewUser() at UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Account;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function userLogin($username, $email, $password) {
-        
+    public function getRegistrationPage()
+    {
+        return "welcome to registration page!";
+    }
+    public function storeNewUser(Request $request)
+    {
+        if ($request->input("request") == "accepted") {
+
+            $newAccount = new Account;
+
+            $newAccount->username = $request->input("username");
+            $newAccount->email = $request->input("email");
+            $newAccount->password = $request->input("password");
+
+            $newAccount->save();
+
+            return response("data successfully inserted into database");
+        } else {
+            return response("duplicate detected");
+        }
+
+
     }
 }
